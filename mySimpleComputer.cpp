480c442e0045e36164ca0165bd7dd699dd0577bc
Aprekek <mhlshamburov@gmail.com>
2020-02-16 16:13:34 +0700

    Aded comands

--- mySimpleComputer.cpp
+++ mySimpleComputer.cpp
@@ -4,11 +4,11 @@
 int main()
 {
     SimpleComputer *smplcmp = SimpleComputer::getInstance();
-    smplcmp->print();
-    std::string filename = "binary.txt";
+    //smplcmp->print();
+    //std::string filename = "binary.txt";
     //smplcmp->memmorySet(5, 133);
     //smplcmp->memmorySave(filename);
-    smplcmp->memmoryLoad(filename);
-    smplcmp->print();
+    //smplcmp->memmoryLoad(filename);
+    //smplcmp->print();
     return 0;
 }
